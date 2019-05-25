747d4a15b0b040b1fab95234b270452e1e352f2a
marvilchis1 <ej.everlake@gmail.com>
2019-05-25 05:55:03 -0500

    Editando el estilo de addproducto, aun sin acabar

--- src/components/Ejemplo.js
+++ src/components/Ejemplo.js
@@ -2,25 +2,57 @@ import React, { Component } from 'react';
 
 export default class Ejemplo extends Component {
     render() {
-        return (
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        return (                
+                        <div className="card text-center shadow-sm  bg-white rounded" Style="width: 100%; heigh">
                 
-                {/*<div class="carousel-inner">
-                    <div class="carousel-item active"> 
-                        <img src="/img/apple.jpg" class="d-block w-100" alt="apple"> </img>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/img/samsung.png" class="d-block w-100" alt="samsung"> </img>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/img/microsoft.jpeg" class="d-block w-100" alt="microsoft"> </img>
-                    </div>*/}
+                            <div className="card-header">
+                                <ul class="nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="http://localhost:3000"> Volver </a>
+                                    </li>
+                                </ul>
+                                <h3>Registro</h3>
+                            </div>
+                            <div className="card-body">
+                                <form action="/users/addUser" method="post">
+                                    <div className="form-group">
+                                        <input type="text" name="nombre" placeholder="Nombre" className="form-control" autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="correo" placeholder="E-Mail" className="form-control" autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="password" name="password" placeholder="Contraseña" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="direccion" placeholder="Direccion" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="ciudad" placeholder="Ciudad" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="estado" placeholder="Estado" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="number" name="cp" placeholder="C.P" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="telefono" placeholder="Telefono" className="form-control"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" name="rfc" placeholder="RFC" className="form-control"/>
+                                    </div>
+                                    <div className = "card-text text-left" >
+                                        <p >Al crear una cuenta, aceptas las Condiciones de Uso y el Aviso de Privacidad de eMarket. </p>
+                                    </div>
+                                    <div className="form-group text-right" >
+                                        <button className="btn btn-success " type= "submit" value="Submit" onClick={() => {alert("Uusario creado")}}>Registrarse</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
 
-                    <div className="card-header">
-                        <h3>Registro</h3>
-                    </div>
-                {/*</div>*/}
-            </div>
+
 
         );
     }
--- src/components/addProducto.js
+++ src/components/addProducto.js
@@ -1,4 +1,7 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+
+
+
 
 export default class Class extends Component {
 
@@ -15,26 +18,70 @@ export default class Class extends Component {
     render() {
         return (
             <div className="AddProducto">
-            <form action="/productos/addProducto" method="post">
-            <h2> Nuevo Producto </h2>
-                <input type="text" className="inputclass" placeholder="User id" name="proveedor_id" defaultValue={this.props.user_id}/>
-                <input type="text" className="inputclass" placeholder="Nombre" name="nombre" />
-                <input type="text" className="inputclass" placeholder="Precio" name="precio" />
-                <input type="text" className="inputclass" placeholder="Descripción" name="descripcion" />
-                <input type="number" className="inputclass" placeholder="Existencia" name="existencia"/>
-                <select name="instructor_id">
-                  {this.state.categorias.map(categoria => (
-                    <option value={categoria.categoria_id}  >{categoria.categoria}</option>
-                  ))}
-                </select>
-                <input type="text" className="inputclass" placeholder="Imagen" name="imagen"/>
-              <div className="SubmitClase">
-                <input type="submit" value="Aceptar" onClick={() => {alert("Clase Creada")}}/>
-              </div>
+
+              <form action="/productos/addProducto" method="post">
+
+                <div class="HubHeader" Style="margin-left: 3rem">
+                  <ul class ="nav nav-pills">
+                    <a class="btn btn-primary" href="http://localhost:3000" role="button"> Volver </a>
+                    <h3 Style="margin-left: 1rem"> Nuevo Producto </h3>
+                  </ul>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="card text-center shadow-sm  bg-white rounded" Style="width: 100%; heigh">
+                      <div className="card-body">
+                        <div clasName="form-group">
+                          <input type="text" name="proveedor_id" placeholder="ID del proveedor" className="form-control" autofocus defaultValue={this.props.proveedor_id}/>
+                        </div>
+                        <input type="text" className="inputclass" placeholder="Nombre" name="nombre" />
+                        <input type="text" className="inputclass" placeholder="Precio" name="precio" />
+                        <input type="text" className="inputclass" placeholder="Descripción" name="descripcion" />
+                        <input type="number" className="inputclass" placeholder="Existencia" name="existencia"/>
+                        <select name="instructor_id">
+                          {this.state.categorias.map(categoria => (
+                            <option value={categoria.categoria_id}  >{categoria.categoria}</option>
+                          ))}
+                        </select>
+                        <input type="text" className="inputclass" placeholder="Imagen" name="imagen"/>
+                      </div>
+                    </div> 
+                  </div>
+                </div>
+                
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="card">
+                      <div className="card-body">
+                        <div clasName="form-group">
+                          <input type="text" name="proveedor_id" placeholder="ID del proveedor" className="form-control" autofocus defaultValue={this.props.proveedor_id}/>
+                        </div>
+                        <input type="text" className="inputclass" placeholder="Nombre" name="nombre" />
+                        <input type="text" className="inputclass" placeholder="Precio" name="precio" />
+                        <input type="text" className="inputclass" placeholder="Descripción" name="descripcion" />
+                        <input type="number" className="inputclass" placeholder="Existencia" name="existencia"/>
+                        <select name="instructor_id">
+                          {this.state.categorias.map(categoria => (
+                            <option value={categoria.categoria_id}  >{categoria.categoria}</option>
+                          ))}
+                        </select>
+                        <input type="text" className="inputclass" placeholder="Imagen" name="imagen"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+             
 
 
-            </form>
-          </div>
+
+                  <div className="SubmitClase">
+                    <input type="submit" value="Aceptar" onClick={() => {alert("Clase Creada")}}/>
+                  </div>
+              </form>
+            </div>
         );
     }
 }
+
+
