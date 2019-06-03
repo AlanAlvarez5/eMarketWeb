cdf0b69874e0c694b51282c288f56f5fbc093fd6
marvilchis1 <ej.everlake@gmail.com>
2019-06-02 20:01:10 -0500

    Vista de Details terminada

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 // import { ProductConsumer } from '../context';
 // import {Link} from 'react-router-dom';
 // import {ButtonContainer} from './Button'
+import styled from 'styled-components';
+
+
 
 export default class Details extends Component {
   
@@ -17,26 +20,77 @@ export default class Details extends Component {
     );
 
     }
+
+
+
+
   
   
   render() {
     
     return (
       
-    <div class="Details">
+      <div class="Details">
+        <HubHeader className = "nav navbar-expand-sm navbar-dark px-sm-5">
+            <div>
+              <a className="btn btn-outline-success" href="http://localhost:3000" role="button"> Volver </a>
+            </div>
+        </HubHeader>
 
-      <div className="HubDeProducto">
-        <h2>{this.state.data.nombre}</h2>
-      </div>
+
+        
+        <div class="container">
+          
+          <div className="row" Style="margin-top: 1rem">
+                                        
+            {/*Hub de la Imagen*/}
+            <div className="col-sm-4">
+                <div className="card shadow-sm bg-white rounded" Style="width: 100%">
+                    <div className="card-body">
+                      <div className="form-group"> 
+                        <img src={this.state.data.imagen} alt="showImagen" class="rounded-lg" Style="width: 100%; height: 100%"/>                    
+                      </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/*Hub de datos del producto (los input(*/}
+            <div className="col-sm-5 ml-auto">
+                <div className="card shadow-sm bg-white rounded" Style="width: 100%">
+                  <div className="card-body">
+                    <InfoProducto>
+                    <h1>{this.state.data.nombre}</h1>
+                    <h4>${this.state.data.precio} </h4>
+
+                    <p> {this.state.data.descripcion}</p>
+
+                    <h7 className="Titles">EXISTENCIA </h7>
+                    <p>{this.state.data.existencia}</p>
+                    </InfoProducto>
+
+                    <div>
+                      <button type="button" className="btn btn-success"> Agregar a carrito </button>
+                    </div>
+                  
+                  </div>
+                </div> 
+            </div>
+                
+
+          </div> {/*row*/}
+
+        </div> {/*Container*/}
+
+
+
+
 
       <div className="InformacionProducto">
-        <p>{this.state.data.proveedor_id}</p>
-        <p>{this.state.data.precio}</p>
-        <p>{this.state.data.descripcion}</p>
         <p>{this.state.data.exitencia}</p>
         <p>{this.state.data.categoria_id}</p>
-        <p>{this.state.data.genero}</p>
       </div>
+
 
     </div>
 
@@ -46,3 +100,19 @@ export default class Details extends Component {
     );
   }
 }
+
+const InfoProducto = styled.div`
+  
+.Titles {
+  font-weight: bold;
+}
+
+`
+
+const HubHeader = styled.nav`
+    background: var(--mainWhite);
+    .nav-link{
+      color: green !important;
+    }
+`
+
